2022e975b4a38b4e6ba4aeb9ce16e51e5beb3921
mepul <sepulvedamorgan2@gmail.com>
2026-02-05 13:57:44 -0600

    1.06 Generate sleep data

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-﻿// ask for input
+// ask for input
 Console.WriteLine("Enter 1 to create data file.");
 Console.WriteLine("Enter 2 to parse data.");
 Console.WriteLine("Enter anything else to quit.");
@@ -15,13 +15,29 @@ if (resp == "1")
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
     int weeks = Convert.ToInt32(Console.ReadLine());
-    
+
     DateTime today = DateTime.Now;
     // we want full weeks sunday - saturday
     DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
     // subtract # of weeks from endDate to get startDate
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    Console.WriteLine(dataDate);
+    Random rnd = new();
+
+    // loop for the desired # of weeks
+    while (dataDate < dataEndDate)
+    {
+        // 7 days in a week
+        int[] hours = new int[7];
+        for (int i = 0; i < hours.Length; i++)
+        {
+            // generate random number of hours slept between 4-12 (inclusive)
+            hours[i] = rnd.Next(4, 13);
+        }
+        // M/d/yyyy,#|#|#|#|#|#|#
+        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        // add 1 week to date
+        dataDate = dataDate.AddDays(7);
+    }
 }
 else if (resp == "2")
 {
